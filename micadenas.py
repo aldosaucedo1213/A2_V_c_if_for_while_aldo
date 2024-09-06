54526ad220a2716d6ef09fa26f55ba57b99e2dea
aldosaucedo1213 <a.22308051281310@cbtis128.edu.mx>
2024-09-06 09:33:22 -0600

    Add files via upload
    subi 2 archivos

---
+++ micadenas.py
@@ -0,0 +1,28 @@
+''' aldo saucedo
+trabajando con cadenas
+'''
+
+nombre="aldo"
+print(nombre[0]) #toma el caracter
+print(nombre)
+
+misnovias=["a,d,s"]
+for chicas in misnovias:
+    print(chicas)
+
+print("otro ejemplo")
+b= "aldo saucedo"
+print(b[2:5])
+print(" a mayusculas")
+a= "aldo saucedo"
+print(a.upper())
+print("concatenar")
+a = "samuel "
+b = "de luna"
+c = a +"_ "+ b
+print(c)
+
+
+
+
+
